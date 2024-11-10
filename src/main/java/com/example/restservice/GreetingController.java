c158ce52035c91dec0acd955024da5016aa11c96
jungwonseok <dnjsehfdld@naver.com>
2024-11-10 15:06:29 +0900

    main test2

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -17,5 +17,5 @@ public class GreetingController {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
     // branch1 test5
-    // main test1
+    // main test2
 }
